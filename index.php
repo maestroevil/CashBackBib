6802351f9955f195a2578fd42b1aaa881a82c749
maestroevil <evgenijgolopotyluk@gmail.com>
2019-09-11 18:44:47 +0300

    MAs
    UP

--- index.php
+++ index.php
@@ -1,1 +1,14 @@
-dsdfsdf
+<? include_once("main/functions.php");
+		
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Bib</title>
+</head>
+<body>
+	<div class="">
+		<a href='MediaSocialElements'> </a>
+	</div>
+</body>
+</html>
