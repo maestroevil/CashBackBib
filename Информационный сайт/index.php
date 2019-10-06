d716dc5bb418c948f3884c7552f625ccc4fbfab9
Edokatawa <55141534+Edokatawa@users.noreply.github.com>
2019-10-06 19:19:12 +0300

    need
    jkhb

---
+++ Информационный сайт/index.php
@@ -0,0 +1,19 @@
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Главная</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+  </head>
+  <body>
+    <header>
+      <nav>
+          <ul>
+            <li><a href="">Главная</a></li>
+            <li><a href=""></a></li>
+            <li><a href="">Команда</a></li>
+            <li><a href="">Контакты</a></li>
+          </ul>
+        </nav>
+    </header>
+  </body>
+</html>
